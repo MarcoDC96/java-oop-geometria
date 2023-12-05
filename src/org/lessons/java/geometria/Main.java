3ca62d018bed2d5983d8a2262fa25a492130054f
MarcoDC96 <marco.decicco@live.it>
2023-12-05 15:21:34 +0100

    Finish with bonus

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,27 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci le dimensioni della base del rettangolo: ");
+        int userBase = scan.nextInt();
+
+        System.out.print("Inserisci le dimensioni dell'altezza del rettangolo: ");
+        int userAltezza = scan.nextInt();
+
+        Rettangolo rettangolo = new Rettangolo(userBase, userAltezza);
+
+        System.out.println("Il perimetro del rettangolo sarà di: " + rettangolo.perimetroRettangolo());
+        System.out.println("L'area del rettangolo sarà di: " + rettangolo.areaRettangolo());
+
+        System.out.println("Disegno rettangolo:");
+        System.out.println(rettangolo.disegna());
+
+
+        scan.close();
+    }
+
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,46 @@
-package org.lessons.java.geometria;public class Rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+    int base;
+    int altezza;
+
+    //Costruttore
+    public Rettangolo(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    //Metodo 1
+
+    int perimetroRettangolo(){
+        return 2*(base+altezza);
+    }
+
+    //Metodo 2
+    int areaRettangolo(){
+        return base * altezza;
+    }
+
+    String disegna (){
+        String newPicture = new String();
+        for (int i = 1; i <= base; i++) {
+            System.out.print("0");
+        }
+        System.out.println();
+        for (int x = 1; x <= altezza - 2; x++) {
+            System.out.print("0");
+            for (int i = 1; i <= base - 2; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("0");
+        }
+        for (int i = 1; i <= base; i++) {
+            System.out.print("0");
+        }
+        return newPicture;
+
+    }
+
+
 }
